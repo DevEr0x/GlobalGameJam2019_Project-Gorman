ec7c08f4a06af597229bd2a144fed2e84d369e8c
Eric Mazza <mcb6010@gmail.com>
2019-01-25 23:11:21 -0500

    Dialouge System

---
+++ GlobalGameJam2019/Assets/Scripts/Dialouge System/DialougeManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialougeManager : MonoBehaviour{
+
+    public Text dialougeName;
+    public Text dialougeText;
+
+    public Animator animator;
+
+    private bool inConversation = false;
+
+    private Queue<string> sentences;
+
+    void Start(){
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialouge(Dialouge dialouge) {
+        Debug.Log("Starting Dialouge with " + dialouge.name);
+        dialougeName.text = dialouge.name;
+        sentences.Clear();
+        inConversation = true;
+        animator.SetBool("inDialouge", true);
+
+        foreach (string sentence in dialouge.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+        public void DisplayNextSentence(){
+            if(sentences.Count == 0){
+                EndDialouge();
+                return;
+            }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+        Debug.Log(sentence);
+        }
+
+    IEnumerator TypeSentence(string sentence){
+        dialougeText.text = "";
+        foreach (char letter in sentence.ToCharArray()){
+            dialougeText.text += letter;
+            yield return null;
+        }
+    }
+
+        void EndDialouge(){
+            Debug.Log("End of conversation.");
+        inConversation = false;
+        animator.SetBool("inDialouge", false);
+    }
+
+    void Update(){
+        if (inConversation){
+            if (Input.GetKeyDown(KeyCode.Space)){
+                DisplayNextSentence();
+            }
+        }
+    }
+}
+
