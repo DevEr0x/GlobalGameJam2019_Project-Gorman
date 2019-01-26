570fa922b257e37cfb38c74f69f1c9899c6627a9
Eric Mazza <mcb6010@gmail.com>
2019-01-26 09:23:58 -0500

    Update PlayerController.cs

--- GlobalGameJam2019/Assets/Scripts/PlayerController.cs
+++ GlobalGameJam2019/Assets/Scripts/PlayerController.cs
@@ -23,39 +23,41 @@ public class PlayerController : MonoBehaviour
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         transform.position += (move * speed * Time.deltaTime);
 
-        if(Input.GetKeyDown(KeyCode.RightArrow)){ //Moving right
+        if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        { //Moving right
             anim.SetBool("isFacingRight", true);
             anim.SetBool("isFacingLeft", false);
             anim.SetBool("isFacingUp", false);
             anim.SetBool("isFacingDown", false);
             anim.SetBool("isWalking", true);
         }
-         if(Input.GetKeyDown(KeyCode.LeftArrow)){ //Moving Left
+         else if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        { //Moving Left
             anim.SetBool("isFacingRight", false);
             anim.SetBool("isFacingLeft", true);
             anim.SetBool("isFacingUp", false);
             anim.SetBool("isFacingDown", false);
             anim.SetBool("isWalking", true);
         }
-         if(Input.GetKeyDown(KeyCode.UpArrow)){ //Moving Up
+         else if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        { //Moving Up
             anim.SetBool("isFacingRight", false);
             anim.SetBool("isFacingLeft", false);
             anim.SetBool("isFacingUp", true);
             anim.SetBool("isFacingDown", false);
             anim.SetBool("isWalking", true);
         }
-        if(Input.GetKeyDown(KeyCode.DownArrow)){ //Moving Down
+        else if(Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        { //Moving Down
             anim.SetBool("isFacingRight", false);
             anim.SetBool("isFacingLeft", false);
             anim.SetBool("isFacingUp", false);
             anim.SetBool("isFacingDown", true);
             anim.SetBool("isWalking", true);
-        }else{
+        }
+        else{
             anim.SetBool("isWalking", false);
         }
-        
-
-
     }
 
 
