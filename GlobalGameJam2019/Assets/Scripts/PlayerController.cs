6964622df68ae5ccfb5ba7a85957a78eeb7d50d9
Eric Mazza <mcb6010@gmail.com>
2019-01-26 07:21:35 -0500

    fuck me

--- GlobalGameJam2019/Assets/Scripts/PlayerController.cs
+++ GlobalGameJam2019/Assets/Scripts/PlayerController.cs
@@ -6,12 +6,15 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D player;
 
+    public Animator anim;
+
     public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
         player = this.GetComponent<Rigidbody2D>();
+        anim = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,6 +22,40 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         transform.position += (move * speed * Time.deltaTime);
+
+        if(move.x > 0){
+            anim.SetBool("facingRight", true);
+            anim.SetBool("facingLeft", false);
+            anim.SetBool("facingUp", false);
+            anim.SetBool("facingDown", false);
+            anim.SetBool("isMoving", true);
+        }
+        else if(move.x < 0){
+            anim.SetBool("facingRight", false);
+            anim.SetBool("facingLeft", true);
+            anim.SetBool("facingUp", false);
+            anim.SetBool("facingDown", false);
+            anim.SetBool("isMoving", true);
+        }
+        else if(move.y > 0){
+            anim.SetBool("facingRight", false);
+            anim.SetBool("facingLeft", false);
+            anim.SetBool("facingUp", true);
+            anim.SetBool("facingDown", false);
+            anim.SetBool("isMoving", true);
+        } else if (move.y < 0){
+            anim.SetBool("facingRight", false);
+            anim.SetBool("facingLeft", false);
+            anim.SetBool("facingUp", false);
+            anim.SetBool("facingDown", true);
+            anim.SetBool("isMoving", true);
+        }
+
+        if(move.x == 0 && move.y == 0)
+        {
+            anim.SetBool("isMoving", false);
+        }
+        
 
 
     }
