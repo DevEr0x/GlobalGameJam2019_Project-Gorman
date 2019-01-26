15105a52cfcccc0c18c4b1ed565cae7ec635c977
Alex DiCarlo <youth3f3r@gmail.com>
2019-01-25 22:20:26 -0500

    BlockManager

---
+++ GlobalGameJam2019/Assets/Scripts/BlockManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour
+{
+    bool dragging = false;
+    Rigidbody2D drag;
+
+    Vector3 mousePos;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Click");
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mouse2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mouse2D, Vector2.zero);
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                if (hit.collider.gameObject.tag == "Draggable")
+                {
+                    if (!dragging)
+                    {
+                        drag = hit.collider.gameObject.GetComponent<Rigidbody2D>();
+                        dragging = true;
+                    }
+
+                }
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            dragging = false;
+            drag = null;
+        }
+        if (dragging)
+        {
+            drag.transform.position = new Vector2(mousePos.x, mousePos.y);
+        }
+    }
+}
--- GlobalGameJam2019/Assets/Scripts/PlayerController.cs
+++ GlobalGameJam2019/Assets/Scripts/PlayerController.cs
@@ -7,9 +7,6 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D player;
 
     public float speed;
-    bool dragging = false;
-    Rigidbody2D drag;
-    Vector3 mousePos;
 
     // Start is called before the first frame update
     void Start()
@@ -20,41 +17,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         transform.position += (move * speed * Time.deltaTime);
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 mouse2D = new Vector2(mousePos.x,mousePos.y);
 
-            RaycastHit2D hit = Physics2D.Raycast(mouse2D,Vector2.zero);
-            if (hit.collider != null)
-            {
-                Debug.Log(hit.collider.gameObject.name);
-                if (hit.collider.gameObject.tag == "Draggable")
-                {
-                    if (!dragging)
-                    {
-                        drag = hit.collider.gameObject.GetComponent<Rigidbody2D>();
-                        dragging = true;
-                    }
-                    
-                }
-            }
-        }else if (Input.GetMouseButtonUp(0))
-        {
-            dragging = false;
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            dragging = false;
-            drag = null;
-        }
-        if  (dragging)
-        {
-            drag.transform.position = new Vector2(mousePos.x, mousePos.y);
-        }
     }
 
 
