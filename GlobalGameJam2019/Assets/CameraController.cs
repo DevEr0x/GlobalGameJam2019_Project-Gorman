efd2a00f9d4fde1eb8aecfe8c5b6ab67f4db626a
Sujan <sujan.sooriyakumar@gmail.com>
2019-01-26 11:29:56 -0500

    map/camera update
    added script to make camera follow player. made janky prototype for first level w/ bounds

---
+++ GlobalGameJam2019/Assets/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float interp = speed * Time.deltaTime;
+
+        if (player.transform.position.x >= 0 && player.transform.position.x <= 22)
+        {
+            Vector3 pos = transform.position;
+            pos.x = Mathf.Lerp(transform.position.x, player.transform.position.x, interp);
+
+            transform.position = pos;
+        }
+    }
+}
