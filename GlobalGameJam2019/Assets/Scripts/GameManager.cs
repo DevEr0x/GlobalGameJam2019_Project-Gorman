455bbba8912b4927aa7cb712a1269ea11b1c90b0
Alex DiCarlo <youth3f3r@gmail.com>
2019-01-26 19:46:50 -0500

    l

--- GlobalGameJam2019/Assets/Scripts/GameManager.cs
+++ GlobalGameJam2019/Assets/Scripts/GameManager.cs
@@ -1,20 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public EnemyPatrol enemy;
     EnemyPatrol inst;
+    public PuzzleManager puzz;
     // Start is called before the first frame update
     void Start()
     {
-    
+        puzz = puzz.GetComponent<PuzzleManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if (puzz.state == PuzzleManager.STATE.BALL)
+        //{
+        //    SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(2));
+        //}
+        //else if (puzz.state == PuzzleManager.STATE.MOP)
+        //{
+        //    SceneManager.LoadScene("Level_03", LoadSceneMode.Additive);
+        //}
+        //else if (puzz.state == PuzzleManager.STATE.RING)
+        //{
+        //    SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(4));
+        //}
+        //else if (puzz.state == PuzzleManager.STATE.HAT)
+        //{
+        //    SceneManager.SetActiveScene(SceneManager.GetSceneByName("End"));
+        //}
     }
 }
--- GlobalGameJam2019/Assets/Scripts/PlayerController.cs
+++ GlobalGameJam2019/Assets/Scripts/PlayerController.cs
@@ -7,8 +7,7 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D player;
 
 
-   Animator anim;
-
+    Animator anim;
     public float speed;
 
     // Start is called before the first frame update
@@ -90,44 +89,7 @@ public class PlayerController : MonoBehaviour
         {
             anim.SetBool("isWalking", false);
         }
-        //Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        //transform.position += (move * speed * Time.deltaTime);
-
-        //if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
-        //{ //Moving right
-        //    anim.SetBool("isFacingRight", true);
-        //    anim.SetBool("isFacingLeft", false);
-        //    anim.SetBool("isFacingUp", false);
-        //    anim.SetBool("isFacingDown", false);
-        //    anim.SetBool("isWalking", true);
-        //}
-        // else if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
-        //{ //Moving Left
-        //    anim.SetBool("isFacingRight", false);
-        //    anim.SetBool("isFacingLeft", true);
-        //    anim.SetBool("isFacingUp", false);
-        //    anim.SetBool("isFacingDown", false);
-        //    anim.SetBool("isWalking", true);
-        //}
-        // else if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
-        //{ //Moving Up
-        //    anim.SetBool("isFacingRight", false);
-        //    anim.SetBool("isFacingLeft", false);
-        //    anim.SetBool("isFacingUp", true);
-        //    anim.SetBool("isFacingDown", false);
-        //    anim.SetBool("isWalking", true);
-        //}
-        //else if(Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
-        //{ //Moving Down
-        //    anim.SetBool("isFacingRight", false);
-        //    anim.SetBool("isFacingLeft", false);
-        //    anim.SetBool("isFacingUp", false);
-        //    anim.SetBool("isFacingDown", true);
-        //    anim.SetBool("isWalking", true);
-        //}
-        //else{
-        //    anim.SetBool("isWalking", false);
-        //}
+        
     }
 
 
